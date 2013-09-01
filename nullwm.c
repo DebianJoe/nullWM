da51eb54f566dddcf767a61a8ffa8f84dac32590
DebianJoe <jbpaladin27@gmail.com>
2013-09-01 07:26:46 -0500

    started maps

--- nullwm.c
+++ nullwm.c
@@ -5,6 +5,8 @@
 /*NOT DONE, DON'T USE YET!!!!*/
 #include <X11/Xlib.h>
 #include <X11/keysym.h>
+#include <stdio.h>
+#include <stdlib.h>
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
 
 int main(void)
@@ -13,15 +15,16 @@ int main(void)
     XWindowAttributes attr;
     XButtonEvent start;
     XEvent ev;
-	unsigned int	modifiers	= ControlMask | Mod1Mask;
+	unsigned int	modifier	= Mod1Mask;
+	KeyCode enter = XKeysymToKeycode(dpy, XStringToKeysym("Enter"));
 
     if(!(dpy = XOpenDisplay(0x0))) return 1;
 
-    XGrabKey(dpy, XKeysymToKeycode(dpy, XStringToKeysym("F1")), modifiers,
+    XGrabKey(dpy, XKeysymToKeycode(dpy, XStringToKeysym("F1")), modifier,
             DefaultRootWindow(dpy), True, GrabModeAsync, GrabModeAsync);
-    XGrabButton(dpy, 1, Mod1Mask, DefaultRootWindow(dpy), True,
+    XGrabButton(dpy, 1, modifier, DefaultRootWindow(dpy), True,
             ButtonPressMask|ButtonReleaseMask|PointerMotionMask, GrabModeAsync, GrabModeAsync, None, None);
-    XGrabButton(dpy, 3, Mod1Mask, DefaultRootWindow(dpy), True,
+    XGrabButton(dpy, 3, modifier, DefaultRootWindow(dpy), True,
             ButtonPressMask|ButtonReleaseMask|PointerMotionMask, GrabModeAsync, GrabModeAsync, None, None);
 
     start.subwindow = None;
@@ -29,6 +32,9 @@ int main(void)
         XNextEvent(dpy, &ev);
         if(ev.type == KeyPress && ev.xkey.subwindow != None)
             XRaiseWindow(dpy, ev.xkey.subwindow);
+		else if(ev.type == KeyPress && ev.xkey.keycode == enter){
+			system("exec urxvt");
+		}
         else if(ev.type == ButtonPress && ev.xbutton.subwindow != None){
             XGetWindowAttributes(dpy, ev.xbutton.subwindow, &attr);
             start = ev.xbutton;
