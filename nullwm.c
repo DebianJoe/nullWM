0d4a07f277844f0510f00a174a6cae6b0cca8f00
DebianJoe <jbpaladin27@gmail.com>
2013-09-01 15:12:51 -0500

    many steps back...

--- nullwm.c
+++ nullwm.c
@@ -3,90 +3,52 @@
  * <DebianJoe@Linuxbbq.org> */
 
 /*NOT DONE, DON'T USE YET!!!!*/
-#include <stdio.h>
-#include <stdlib.h>
-#include <xcb/xcb.h>
+#include <X11/Xlib.h>
 
-int main (int argc, char **argv)
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+
+int main()
 {
+    Display * dpy;
+    Window root;
+    XWindowAttributes attr;
+    XButtonEvent start;
+    XEvent ev;
 
-    uint32_t values[3];
-    xcb_connection_t *display;
-    xcb_screen_t *screen;
-    xcb_drawable_t win;
-    xcb_drawable_t root;
-    xcb_generic_event_t *ev;
-    xcb_get_geometry_reply_t *geom;
+    if(!(dpy = XOpenDisplay(0x0))) return 1;
 
-    display = xcb_connect(NULL, NULL);
-    if (xcb_connection_has_error(display)) return 1;
+    root = DefaultRootWindow(dpy);
 
-    screen = xcb_setup_roots_iterator(xcb_get_setup(display)).data;
-    root = screen->root;
+    XGrabKey(dpy, XKeysymToKeycode(dpy, XStringToKeysym("F1")), Mod1Mask, root,
+            True, GrabModeAsync, GrabModeAsync);
+    XGrabButton(dpy, 1, Mod1Mask, root, True, ButtonPressMask, GrabModeAsync,
+            GrabModeAsync, None, None);
+    XGrabButton(dpy, 3, Mod1Mask, root, True, ButtonPressMask, GrabModeAsync,
+            GrabModeAsync, None, None);
 
-    xcb_grab_key(display, 1, root, XCB_MOD_MASK_2, XCB_NO_SYMBOL,
-                 XCB_GRAB_MODE_ASYNC, XCB_GRAB_MODE_ASYNC);
-
-    xcb_grab_button(display, 0, root, XCB_EVENT_MASK_BUTTON_PRESS | 
-                XCB_EVENT_MASK_BUTTON_RELEASE, XCB_GRAB_MODE_ASYNC, 
-                XCB_GRAB_MODE_ASYNC, root, XCB_NONE, 1, XCB_MOD_MASK_1);
-
-    xcb_grab_button(display, 0, root, XCB_EVENT_MASK_BUTTON_PRESS | 
-                XCB_EVENT_MASK_BUTTON_RELEASE, XCB_GRAB_MODE_ASYNC, 
-                XCB_GRAB_MODE_ASYNC, root, XCB_NONE, 3, XCB_MOD_MASK_1);
-    xcb_flush(display);
-
-    for (;;){
-        ev = xcb_wait_for_event(display);
-        switch (ev->response_type & ~0x80) {
-        
-        case XCB_BUTTON_PRESS:{
-            xcb_button_press_event_t *e;
-            e = ( xcb_button_press_event_t *) ev;
-            win = e->child; 
-            values[0] = XCB_STACK_MODE_ABOVE;
-            xcb_configure_window(display, win, XCB_CONFIG_WINDOW_STACK_MODE, values);
-            geom = xcb_get_geometry_reply(display, xcb_get_geometry(display, win), NULL);
-            if (1 == e->detail) {
-                values[2] = 1; 
-                xcb_warp_pointer(display, XCB_NONE, win, 0, 0, 0, 0, 1, 1);
-            } else {
-                values[2] = 3; 
-                xcb_warp_pointer(display, XCB_NONE, win, 0, 0, 0, 0, geom->width, geom->height);
-            }
-            xcb_grab_pointer(display, 0, root, XCB_EVENT_MASK_BUTTON_RELEASE
-                    | XCB_EVENT_MASK_BUTTON_MOTION | XCB_EVENT_MASK_POINTER_MOTION_HINT, 
-                    XCB_GRAB_MODE_ASYNC, XCB_GRAB_MODE_ASYNC, root, XCB_NONE, XCB_CURRENT_TIME);
-            xcb_flush(display);
+    for(;;){
+        XNextEvent(dpy, &ev);
+        if(ev.type == KeyPress && ev.xkey.subwindow != None)
+            XRaiseWindow(dpy, ev.xkey.subwindow);
+        else if(ev.type == ButtonPress && ev.xbutton.subwindow != None){
+            XGrabPointer(dpy, ev.xbutton.subwindow, True,
+                    PointerMotionMask|ButtonReleaseMask, GrabModeAsync,
+                    GrabModeAsync, None, None, CurrentTime);
+            XGetWindowAttributes(dpy, ev.xbutton.subwindow, &attr);
+            start = ev.xbutton;
         }
-        break;
-
-        case XCB_MOTION_NOTIFY:{
-            xcb_query_pointer_reply_t *pointer;
-            pointer = xcb_query_pointer_reply(display, xcb_query_pointer(display, root), 0);
-            if (values[2] == 1) {/* move */
-                geom = xcb_get_geometry_reply(display, xcb_get_geometry(display, win), NULL);
-                values[0] = (pointer->root_x + geom->width > screen->width_in_pixels)?
-                    (screen->width_in_pixels - geom->width):pointer->root_x;
-                values[1] = (pointer->root_y + geom->height > screen->height_in_pixels)?
-                    (screen->height_in_pixels - geom->height):pointer->root_y;
-                xcb_configure_window(display, win, XCB_CONFIG_WINDOW_X | XCB_CONFIG_WINDOW_Y, values);
-                xcb_flush(display);
-            } else if (values[2] == 3) { /* resize */
-                geom = xcb_get_geometry_reply(display, xcb_get_geometry(display, win), NULL);
-                values[0] = pointer->root_x - geom->x;
-                values[1] = pointer->root_y - geom->y;
-                xcb_configure_window(display, win, XCB_CONFIG_WINDOW_WIDTH | XCB_CONFIG_WINDOW_HEIGHT, values);
-                xcb_flush(display);
-            }
+        else if(ev.type == MotionNotify){
+            int xdiff, ydiff;
+            while(XCheckTypedEvent(dpy, MotionNotify, &ev));
+            xdiff = ev.xbutton.x_root - start.x_root;
+            ydiff = ev.xbutton.y_root - start.y_root;
+            XMoveResizeWindow(dpy, ev.xmotion.window,
+                attr.x + (start.button==1 ? xdiff : 0),
+                attr.y + (start.button==1 ? ydiff : 0),
+                MAX(1, attr.width + (start.button==3 ? xdiff : 0)),
+                MAX(1, attr.height + (start.button==3 ? ydiff : 0)));
         }
-        break;
-
-        case XCB_BUTTON_RELEASE:
-            xcb_ungrab_pointer(display, XCB_CURRENT_TIME);
-            xcb_flush(display); 
-        break;
-        }
+        else if(ev.type == ButtonRelease)
+            XUngrabPointer(dpy, CurrentTime);
     }
-return 0;
 }
