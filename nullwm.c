2024e0659effb4dd924b397f9a3ef845121393ea
DebianJoe <jbpaladin27@gmail.com>
2013-09-02 07:19:09 -0500

    3 more steps back

--- nullwm.c
+++ nullwm.c
@@ -1,58 +1,45 @@
-/* nullWM is a very simple stacking window manager
- * inspired by tinyWM and written by Joe Brock
- * <DebianJoe@Linuxbbq.org> */
-
-/*NOT DONE, DON'T USE YET!!!!*/
 #include <X11/Xlib.h>
 #include <X11/keysym.h>
 
-Display *disp;
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
 
-int main()
+int main(void)
 {
-    //Display * disp;
-    Window root;
+    Display * disp;
     XWindowAttributes attr;
     XButtonEvent start;
     XEvent ev;
 
     if(!(disp = XOpenDisplay(0x0))) return 1;
 
-    root = DefaultRootWindow(disp);
+    XGrabKey(disp, XKeysymToKeycode(disp, XStringToKeysym("F1")), Mod1Mask,
+            DefaultRootWindow(disp), True, GrabModeAsync, GrabModeAsync);
+    XGrabButton(disp, 1, Mod1Mask, DefaultRootWindow(disp), True,
+            ButtonPressMask|ButtonReleaseMask|PointerMotionMask, GrabModeAsync, GrabModeAsync, None, None);
+    XGrabButton(disp, 3, Mod1Mask, DefaultRootWindow(disp), True,
+            ButtonPressMask|ButtonReleaseMask|PointerMotionMask, GrabModeAsync, GrabModeAsync, None, None);
 
-    XGrabKey(disp, XKeysymToKeycode(disp, XK_Shift_L), Mod1Mask, root,
-	     True, GrabModeAsync, GrabModeAsync);
-    XGrabKey(disp, XKeysymToKeycode(disp, XK_Return), Mod1Mask, root,
-	     True, GrabModeAsync, GrabModeAsync);
-    XGrabButton(disp, 1, Mod1Mask, root, True, ButtonPressMask, GrabModeAsync,
-            GrabModeAsync, None, None);
-    XGrabButton(disp, 3, Mod1Mask, root, True, ButtonPressMask, GrabModeAsync,
-            GrabModeAsync, None, None);
-
-    for(;;){
+    start.subwindow = None;
+    for(;;)
+    {
         XNextEvent(disp, &ev);
         if(ev.type == KeyPress && ev.xkey.subwindow != None)
             XRaiseWindow(disp, ev.xkey.subwindow);
         else if(ev.type == ButtonPress && ev.xbutton.subwindow != None){
-            XGrabPointer(disp, ev.xbutton.subwindow, True,
-                    PointerMotionMask|ButtonReleaseMask, GrabModeAsync,
-                    GrabModeAsync, None, None, CurrentTime);
             XGetWindowAttributes(disp, ev.xbutton.subwindow, &attr);
             start = ev.xbutton;
         }
-        else if(ev.type == MotionNotify){
-            int xdiff, ydiff;
-            while(XCheckTypedEvent(disp, MotionNotify, &ev));
-            xdiff = ev.xbutton.x_root - start.x_root;
-            ydiff = ev.xbutton.y_root - start.y_root;
-            XMoveResizeWindow(disp, ev.xmotion.window,
+        else if(ev.type == MotionNotify && start.subwindow != None){
+            int xdiff = ev.xbutton.x_root - start.x_root;
+            int ydiff = ev.xbutton.y_root - start.y_root;
+            XMoveResizeWindow(disp, start.subwindow,
                 attr.x + (start.button==1 ? xdiff : 0),
                 attr.y + (start.button==1 ? ydiff : 0),
                 MAX(1, attr.width + (start.button==3 ? xdiff : 0)),
                 MAX(1, attr.height + (start.button==3 ? ydiff : 0)));
         }
-        else if(ev.type == ButtonRelease)
-            XUngrabPointer(disp, CurrentTime);
+        else if(ev.type == ButtonRelease){
+            start.subwindow = None;
+        }
     }
 }
